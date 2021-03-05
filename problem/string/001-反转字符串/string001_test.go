fb50bd6d7fdf8ab9b3e749f3a86d315985b6a533
jerome0000 <geekerjohn@163.com>
2021-03-06 00:12:20 +0800

    反转字符串

---
+++ problem/string/001-反转字符串/string001_test.go
@@ -0,0 +1,26 @@
+package string
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_reverseString(t *testing.T) {
+	type args struct {
+		s []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{"test1", args{s: []byte{1, 2, 3, 4}}, []byte{4, 3, 2, 1}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseString(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
