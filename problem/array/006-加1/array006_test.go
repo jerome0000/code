93cc9a3381a021d186c2c5e36a33bd74c242cdad
jerome0000 <geekerjohn@163.com>
2021-03-06 15:50:41 +0800

    加1

---
+++ problem/array/006-加1/array006_test.go
@@ -0,0 +1,27 @@
+package array
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_plusOne(t *testing.T) {
+	type args struct {
+		digits []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"test1", args{digits: []int{1, 2, 3}}, []int{1, 2, 4}},
+		{"test2", args{digits: []int{1, 3, 9}}, []int{1, 4, 0}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := plusOne(tt.args.digits); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("plusOne() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
