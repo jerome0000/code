3e16c90f6c75b9fe096853415707ec7eaa88b425
jerome0000 <geekerjohn@163.com>
2021-03-06 11:16:37 +0800

    最大子序和

---
+++ problem/dynamic/002-最大子序和/dynamic002_test.go
@@ -0,0 +1,23 @@
+package dynamic
+
+import "testing"
+
+func Test_maxSubArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test", args{nums: []int{1, 2, 3, 4, -1}}, 10},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxSubArray(tt.args.nums); got != tt.want {
+				t.Errorf("maxSubArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
