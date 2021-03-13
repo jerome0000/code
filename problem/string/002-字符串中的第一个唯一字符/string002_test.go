d2cb816a177682286a0cac4f1d3b9e09337a8f83
jerome0000 <geekerjohn@163.com>
2021-03-13 15:17:46 +0800

    字符串中的第一个唯一字符

---
+++ problem/string/002-字符串中的第一个唯一字符/string002_test.go
@@ -0,0 +1,24 @@
+package string
+
+import "testing"
+
+func Test_firstUniqChar(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"test1", args{s: string("abcdefabcde")}, 5},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := firstUniqChar(tt.args.s); got != tt.want {
+				t.Errorf("firstUniqChar() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
