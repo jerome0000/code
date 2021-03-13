2c52f00106bd105c8ec780d9da137a76f97cc168
jerome0000 <geekerjohn@163.com>
2021-03-13 14:33:11 +0800

    三角形最小路径和

---
+++ problem/dynamic/004-三角形最小路径和/dynamic004_test.go
@@ -0,0 +1,23 @@
+package array
+
+import "testing"
+
+func Test_minimumTotal(t *testing.T) {
+	type args struct {
+		triangle [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minimumTotal(tt.args.triangle); got != tt.want {
+				t.Errorf("minimumTotal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
