7f488d8a3799a83d32ab402d9f84d8450ca5733f
jerome0000 <geekerjohn@163.com>
2021-03-13 15:01:14 +0800

    打家劫舍

---
+++ problem/dynamic/006-打家劫舍/dynamic006_test.go
@@ -0,0 +1,24 @@
+package dynamic
+
+import "testing"
+
+func Test_rob(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"test1", args{nums: []int{1, 2, 3, 4, 5, 6}}, 12},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rob(tt.args.nums); got != tt.want {
+				t.Errorf("rob() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
