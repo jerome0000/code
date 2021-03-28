0ced2a0aa9abf2cf36bb3b5df9bacbf0758938af
jerome0000 <geekerjohn@163.com>
2021-03-28 14:38:40 +0800

    二叉树的最大深度

---
+++ problem/tree/002-二叉树的最大深度/tree002_test.go
@@ -0,0 +1,22 @@
+package tree
+
+import "testing"
+
+func Test_maxDepth(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxDepth(tt.args.root); got != tt.want {
+				t.Errorf("maxDepth() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
